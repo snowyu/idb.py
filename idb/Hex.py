5364926b70730cba5511c037e411e67ccc3b5dff
Riceball LEE <snowyu.lee@gmail.com>
2012-08-04 19:39:42 +0800

    - remove unecessary import in Hex.

--- idb/Hex.py
+++ idb/Hex.py
@@ -1,15 +1,6 @@
 # The Hex / KeyValue Class(Spec v0.2):
 
-import os
-import collections
-
-from os import path
-
-from utils import CreateDir, Str2Int, SetXattrValue, GetXattrValue
-from helpers import IDB_SPEC_VER, WriteFileValueToCache, ReadFileValueFromCache, DeleteDBValue, iDBError
-from helpers import EIDBNODIR, IDB_VALUE_NAME, IDB_KEY_TYPE_NAME
 from Integer import Integer
-from Item import Item
 
 class Hex(Integer):
     #def __new__(cls,  aData, **kwargs):
